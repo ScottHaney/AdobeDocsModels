18bddc7ed3c6dd94c611bdca38462628a5400eab
Scott Haney <emailalmondbranch@gmail.com>
2023-12-17 08:53:40 -0800

    Add in a few more methods

--- AdobeDocsModels/AdobeDocsModels/ObjectParser.cs
+++ AdobeDocsModels/AdobeDocsModels/ObjectParser.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace AdobeDocsModels;
 
 public class ObjectParser
 {
+
+
+    private string GetFullText(XElement element)
+    {
+        var innerText = element.Value;
+
+        var regex = new Regex("<.*?>");
+        return regex.Replace(innerText, x => string.Empty);
+    }
+
+    private SectionItem ReadSections(XElement section)
+    {
+        var children = section.Elements().ToList();
+
+        List<XElement> items = [];
+        List<XElement> sections = [];
+        foreach (var child in children)
+        {
+            if (child.Name == "section")
+                sections.Add(child);
+            else
+                items.Add(child);
+        }
+
+        return new(items, sections.Select(x => ReadSections(x)).ToList());
+    }
 }
+
+public record SectionItem(ICollection<XElement> Items, ICollection<SectionItem> Sections);
--- AdobeDocsModels/AdobeDocsModels/Program.cs
+++ AdobeDocsModels/AdobeDocsModels/Program.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("config.json", true)
     .Build();
+
+var applicationXmlPath = Path.Combine("XmlDocs", "general", "application.xml");
+
+var root = XElement.Load(applicationXmlPath);
+
+var objectSection = root.XPathSelectElement("./section").Value;
+var attributesSection = root.XPathSelectElement("./section/section[@ids='attributes']");
+var methodsSection = root.XPathSelectElement("./section/section[@ids='methods']");
 
 var builder = Host.CreateDefaultBuilder(args);
 
