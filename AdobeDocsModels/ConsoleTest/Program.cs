b3361d5b9dae5c94205ec4545c5bb4e88922fb2c
Scott Haney <emailalmondbranch@gmail.com>
2024-01-01 09:33:41 -0800

    Get the main test program working again

--- AdobeDocsModels/ConsoleTest/Program.cs
+++ AdobeDocsModels/ConsoleTest/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdobeDocsModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("config.json", true)
+    .Build();
+
+var applicationXmlPath = Path.Combine("XmlDocs", "general", "application.xml");
+
+var root = XElement.Load(applicationXmlPath);
+var parser = new SectionsParser();
+
+var result = parser.ReadAllSections(root);
+var builder = Host.CreateDefaultBuilder(args);
+
+await builder.RunConsoleAsync();
